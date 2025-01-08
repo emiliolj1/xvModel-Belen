05839c2697b238e8400bbbb75cb69b033a9f17e1
Emilio Luna Jandar <lunaemilio269@gmail.com>
2025-01-08 15:32:12 -0300

    Changed html name, fonts, colors and name

--- src/components/layout/Countdown.jsx
+++ src/components/layout/Countdown.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import "../styles/countDown.css"
 
 function Countdown() {
-  const [countDownDate, setCountDownDate] = useState(new Date('Nov 15, 2024 22:30:00').getTime());
+  const [countDownDate, setCountDownDate] = useState(new Date('Jan 25, 2025 22:30:00').getTime());
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
