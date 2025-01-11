2b77c9f546cb2cd882f1253e63fa3899c9fb585f
Emilio Luna Jandar <lunaemilio269@gmail.com>
2025-01-11 19:49:22 -0300

    Added meta tag in index.html

--- src/components/sections/Six.jsx
+++ src/components/sections/Six.jsx
@@ -8,7 +8,7 @@ const Six = () => {
         <p className='mb-0'>Hoy comienza un capítulo nuevo lleno de alegría y esperanza. </p>
         <p className='mb-0'>Gracias por acompañarme en este día tan especial</p>
         {/* <p className='mb-0'>merecen ser compartidos...</p> */}
-        <p className='titulo mt-2' style={{fontSize:'2.5rem'}}>Belen</p>
+        <p className='titulo mt-2' style={{fontSize:'2.5rem'}}>Belén</p>
       </div>
     </Container>
   )
