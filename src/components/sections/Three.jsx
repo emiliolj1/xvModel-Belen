627398c5d9572576bf00ecc272cdd39a5c120b31
Emilio Luna Jandar <lunaemilio269@gmail.com>
2025-01-10 22:48:12 -0300

    Added Iglesia Picture

--- src/components/sections/Three.jsx
+++ src/components/sections/Three.jsx
@@ -14,14 +14,14 @@ const Three = () => {
         <div className='card cardFont mx-5 px-3 py-3 text-center'>
           <Image
             className='rounded-4 p-3'
-            src='https://i.imgur.com/q26FauV.jpeg'
+            src='https://i.imgur.com/bweNlA3.png'
           />
-          <h3 className='fw-bold m-0 fs-4'>Parroquia</h3>
-          <h3 className='fw-bold mt-0 fs-4'>'Nstra. Sra. del Valle' </h3>
+          <h3 className='fw-bold m-0 fs-4'>Capilla</h3>
+          <h3 className='fw-bold mt-0 fs-4'>'Sagrado Corazon de Jesus'</h3>
           <p className='mb-2'><GeoAltFill/> Av. Torquinst - La Florida</p>
-          <p className='fw-bold mb-1'>3 · Noviembre · 2024</p>
-          <p className='fw-bold mb-1'>19:30hrs</p>
-          <Button href='https://maps.app.goo.gl/9KzMrta4uDznnu4f8' className='mb-3 mx-auto cardButton fw-bold'>
+          <p className='fw-bold mb-1'>25 · Enero · 2025</p>
+          <p className='fw-bold mb-1'>20:00hrs</p>
+          <Button href='https://maps.app.goo.gl/tqtiP9j1AF5TzVWz8' className='mb-3 mx-auto cardButton fw-bold'>
             Ver mapa!
           </Button>
         </div>
@@ -29,12 +29,11 @@ const Three = () => {
           <h3 className='fw-bold' style={{fontSize:'2.4rem', fontStyle: 'normal'}}>¡Fiesta!</h3>
         </div>
         <div className='card cardFont mx-5 px-2 py-2 text-center'>
-          <h3 className='fw-bold pt-3 fs-4'>Salon Coyote</h3>
-          <p className='mb-2'><GeoAltFill/> Av. M. T. Alvear - Delfín Gallo</p>
-          <p className='fw-bold mb-1'>15 · Noviembre · 2024</p>
-          <p className='fw-bold mb-1'>22:00hrs</p>
-          <p className='fw-bold mb-2'>¡Sé puntual!</p>
-          <Button href='https://maps.app.goo.gl/22Ygrp85hBdgkP2j8' className='mb-3 mx-auto cardButton fw-bold'>
+          <h3 className='fw-bold pt-3 fs-4'>Mi Casa!</h3>
+          <p className='mb-2'><GeoAltFill/> B Delfin Gallo - Delfín Gallo</p>
+          <p className='fw-bold mb-1'>25 · Enero · 2025</p>
+          <p className='fw-bold mb-1'>¡Despues de la misa!</p>
+          <Button href='https://maps.app.goo.gl/vYhYnuTmSYe3z18e7' className='mb-3 mx-auto cardButton fw-bold'>
             Ver mapa!
           </Button>
         </div>
